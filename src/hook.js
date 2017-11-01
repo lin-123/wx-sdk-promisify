ae406524ead0a96bf331934b5ad14eed2f581681
侯林江 <linjiang.hou@baidao.com>
2017-11-01 16:29:35 +0800

    fix previewImage of hook: check arguments valid

--- src/hook.js
+++ src/hook.js
@@ -1,5 +1,5 @@
 // 图片预览只接受http的url链接[见微信官方API]
 export const previewImage = ({current, urls}) => {
-  const toHttp = (url) => url.replace(/^https*/, 'http')
-  return {current: toHttp(current), urls: urls.map(toHttp)}
+  const toHttp = (url) => url && url.replace(/^https*/, 'http')
+  return {current: toHttp(current), urls: urls && urls.length && urls.map(toHttp)}
 }
